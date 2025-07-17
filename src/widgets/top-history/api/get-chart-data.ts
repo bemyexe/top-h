0727ce1c49b069a6d17020d00d6c9e06dd4a5bec
maxim <trifonov.max@mail.ru>
2025-07-17 21:17:11 +0300

    feat: chartdata hook

---
+++ src/widgets/top-history/api/get-chart-data.ts
@@ -0,0 +1,16 @@
+import {useQuery} from '@tanstack/react-query';
+
+import {type Country, topHistoryApi} from './api';
+
+export const useChartData = (
+  countryId: Country['id'],
+  dateFrom: string,
+  dateTo: string
+) => {
+  const {data, isLoading, error} = useQuery({
+    queryKey: ['chart', 'data', countryId, dateFrom, dateTo],
+    queryFn: () => topHistoryApi.getChartData(countryId, dateFrom, dateTo),
+  });
+
+  return {data, isLoading, error};
+};
